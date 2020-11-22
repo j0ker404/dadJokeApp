7918d386ae936f48bc7aafe1613682e5f7189cc6
joker <uanhermanus@gmail.com>
2020-11-22 21:10:34 +0200

    integrate cloud firestore

--- src/components/Joke.vue
+++ src/components/Joke.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <div @click="updateJoke" class="fold page">
       <div class="txt-body">
-        {{ txt }}
+        {{ txt.text }}
       </div>
     </div>
   </div>
@@ -10,40 +10,67 @@
 
 <script>
 import axios from "axios";
-
+import firebase from "../firebaseConfig";
+const db = firebase.firestore();
 export default {
   name: "Joke",
   data() {
     return {
-      txt: null,
+      txt: {
+        id: -1,
+        text: "Joke Pending",
+      },
+      jokes: [],
     };
   },
   methods: {
     updateJoke() {
       console.log("Gettting new joke");
+      let temp = 0;
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        temp = Math.floor(Math.random() * this.jokes.length);
+        // console.log(this.jokes[temp].id);
+        // console.log(this.txt.id);
+        if (this.txt.id !== this.jokes[temp].id) break;
+      }
+      // console.log(temp);
+      this.txt = this.jokes[temp];
+      // console.log(this.txt);
     },
     getJoke() {
       axios
         .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-        .then((response) => (this.txt = response))
+        .then((response) => (this.txt = response.data.bpi))
         .catch((err) => {
           console.log(err);
-          this.txt = "Joke Pending";
+          this.txt = "try again later";
+        });
+    },
+    getJokes() {
+      this.jokes = [];
+      db.collection("jokes")
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((doc) => {
+            this.jokes.push({
+              id: doc.id,
+              text: doc.data().text,
+            });
+            // console.log(doc.id, " => ", doc.data().text);
+          });
+          this.updateJoke();
+        })
+        .catch((error) => {
+          console.log("Error", error);
         });
     },
   },
   mounted() {
-      axios
-        .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-        .then((response) => (this.txt = response))
-        .catch((err) => {
-          console.log(err);
-          this.txt = "Joke Pending";
-        });
+    // this.getJoke();
+    this.getJokes();
   },
 };
-
-
 </script>
 
 <style scoped>
@@ -64,7 +91,7 @@ export default {
 
 .txt-body {
   color: coral;
-  font-size: 1.2em;
+  font-size: 2em;
 }
 
 /* 
@@ -74,10 +101,6 @@ https://codepen.io/muhamidelsayed/pen/rjyMaM
 /* paper */
 .fold {
   height: 100%;
-  /* margin: 1px; */
-  /* position: absolute; */
-  /* height: 200px; */
-  /* padding: 25px; */
   background: #fff;
 }
 /* folding */
@@ -97,7 +120,7 @@ https://codepen.io/muhamidelsayed/pen/rjyMaM
   transition: all ease 0.5s;
 }
 .fold:hover::before {
-  border-width: 0 10em 10em 0;
+  border-width: 0 1.5em 1.5em 0;
   border-color: #eee #344;
 }</style
 >>
